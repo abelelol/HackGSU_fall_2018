033e44d98dd2e648272657901579195560d573e8
abelelol <abeleamsalu@gmail.com>
2018-10-06 04:07:59 -0400

    added first comment

--- app/src/main/java/ncr_project/com/ncr_project/MainActivity.java
+++ app/src/main/java/ncr_project/com/ncr_project/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //first method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
